669a6f361bded928e374d1d2cdea141b131b6006
Alex Li <alexhome355@gmail.com>
2020-03-22 13:32:49 -0700

    Update the move method of the Chess class.
    This method checks whether or not a player can make a move (determined by the current game state, whether the arguments passed are out of range, and if move adhere's to that piece's rules). For example, if game is already finished or player is trying to move one of the opponent's pieces then obviously move is invalid.

--- game.py
+++ game.py
@@ -8,34 +8,67 @@ class Chess:
     def __init__(self):
         self.game_board = Board()
         self.board = self.game_board.board
-        self.turn = 'white'
+        self.turn = 'w'
+        self.state = 'UNFINISHED'
 
     def move(self, row, col, new_row, new_col):
-        piece = self.board[row][col]
+        """Moves specified piece to the specified location on board if valid.
 
-        if piece.is_move_valid(new_row, new_col, self.board) is True:
-            self.board[new_row][new_col] = piece
-            self.board[row][col] = '_'
-            piece.row = new_row
-            piece.col = new_col
-            return True
-        else:
-            return piece.is_move_valid(new_row, new_col, self.board)
+        Keyword arguments:
+        row -- the row on the game board that piece is located
+        col -- the column on the game board that piece is located
+        new_row -- the row on the game board that piece is trying to move to
+        new_col -- the column on the game board that piece is trying to move to
+        """
+        # Returns False if game is already finished.
+        if self.state != 'UNFINISHED':
+            return False
+
+        # Returns False if the arguments passed are out of range.
+        if row not in range(8) or col not in range(8) or \
+                new_row not in range(8) or new_col not in range(8):
+            return False
+
+        # Returns False if starting position contains no piece to move.
+        if self.board[row][col] == '_':
+            return False
+
+        # Returns False if starting and ending positions are the same.
+        if new_row == row and new_col == col:
+            return False
+
+        # Returns False if player tries to move opponent's piece or capture one
+        # of its own pieces.
+        if self.turn == 'w':
+            if self.board[row][col].color == 'b':
+                return False
+            if self.board[new_row][new_col] != '_':
+                if self.board[new_row][new_col].color == 'w':
+                    return False
+        if self.turn == 'b':
+            if self.board[row][col].color == 'w':
+                return False
+            if self.board[new_row][new_col] != '_':
+                if self.board[new_row][new_col].color == 'b':
+                    return False
+
+        # Returns False if move is invalid per that piece type's rules.
+        piece = self.board[row][col]
+        taken_piece = self.board[new_row][new_col]
+        if piece.is_move_valid(new_row, new_col, self.board) is False:
+            return False
+
+        # Updates the position of the piece.
+        self.board[new_row][new_col] = piece
+        self.board[row][col] = '_'
+        piece.row = new_row
+        piece.col = new_col
+
+        return True
 
 
 
 a = Chess()
 a.game_board.print_board()
-print(a.move(7, 3, 6, 2))
+print(a.move(6, 3, 3, 3))
 a.game_board.print_board()
-print(a.move(6, 2, 5, 3))
-a.game_board.print_board()
-print(a.move(5, 3, 3, 5))
-a.game_board.print_board()
-print(a.move(7, 4, 6, 4))
-a.game_board.print_board()
-print(a.move(6, 4, 5, 4))
-a.game_board.print_board()
-print(a.move(5, 4, 6, 6))
-a.game_board.print_board()
-print('hi!')
